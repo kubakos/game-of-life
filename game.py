6490c305ef8dd8b4baeaf23574b5cfabed82ec65
Akos Kubak <kubakakos@gmail.com>
2023-11-10 13:53:32 +0100

    set_population method check

--- game.py
+++ game.py
@@ -4,18 +4,21 @@ import numpy as np
 class GameOfLife:
 
     def __init__(self):
-        self.map_width = 100
         self.map_height = 100
-        self.map_size = [self.map_width, self.map_height]
-        self.population = np.zeros(self.map_size, dtype=np.int8)
+        self.map_width = 100
+        self.map_size = [self.map_height, self.map_width]
+        self.population = np.random.randint(0, 1, self.map_size)
 
-    def set_map_size(self, width, height):
-        self.map_width = width
+    def set_map_size(self, height, width):
         self.map_height = height
-        self.population = np.zeros(self.map_size, dtype=np.int8)
+        self.map_width = width
+        self.population = np.random.randint(0, 1, self.map_size)
 
-    def set_population(self, popsize):
-        pass
+    def set_population(self, population):
+        if population == 'random':
+            self.population = np.random.randint(0, 2, self.map_size)
+        elif isinstance(population, np.ndarray):
+            self.population = population
 
     def play(self):
         pass
