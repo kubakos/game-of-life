f2c0119df55b9868328c4eb8501a9d86f456bd42
Akos Kubak <kubakakos@gmail.com>
2023-11-14 18:03:26 +0100

    update

--- game.py
+++ game.py
@@ -5,9 +5,8 @@ import os
 
 class GameOfLife:
 
-    def __init__(self, height=100, width=100):
-        self.generations = 10000
-        self.map_size = [height, width]
+    def __init__(self):
+        self.map_size = [10, 10]
         self.population_density = 2
         self.population = np.random.randint(
             0, self.population_density, self.map_size)
@@ -18,23 +17,29 @@ class GameOfLife:
     def set_map_size(self, height, width):
         if isinstance(height, int) & isinstance(width, int):
             self.map_size = [height, width]
+            print("Map size set!")
+        else:
+            print("Map size couldn't be set!")
 
     def set_population(self, population):
         if isinstance(population, np.ndarray):
             self.population = population
-
-    def set_generations(self, gens):
-        if isinstance(gens, int):
-            self.generations = gens
+            print("Population map set!")
+        else:
+            print("Population map couldn't be set!")
 
     def set_density(self, pd):
-        self.population_density = pd
+        if isinstance(pd, int):
+            self.population_density = pd
+            print("Population density registered!")
+        else:
+            print("Population density didn't register!")
 
     def is_alive(self, cell):
         neighbours = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
-                if (i or j != 0) & (cell[0] + i >= 0) & (cell[1] + j >= 0):
+                if (i or j != 0) & (0 <= cell[0] + i <= self.map_size[0] - 1) & (0 <= cell[1] + j <= self.map_size[1] - 1):
                     neighbours += self.population[cell[0] + i, cell[1] + j]
 
         if self.population[cell[0], cell[1]] == 1:
@@ -65,15 +70,17 @@ class GameOfLife:
 
     def start(self):
         tmp_population = np.zeros(self.map_size, np.int8)
-        for gen in range(self.generations):
-            for i in range(self.map_size[0] - 1):
-                for j in range(self.map_size[1] - 1):
+        generation = 0
+        while True:
+            for i in range(self.map_size[0]):
+                for j in range(self.map_size[1]):
                     if self.is_alive([i, j]) is True:
                         tmp_population[i, j] = 1
                     else:
                         tmp_population[i, j] = 0
             os.system('clear')
+            generation += 1
             self.population = tmp_population
             print(*self.transform_to_str(self.population), sep='\n')
-            print(gen)
+            print(generation)
             time.sleep(1/30)
--- main.py
+++ main.py
@@ -2,5 +2,7 @@
 import game
 
 if __name__ == '__main__':
-    x = game.GameOfLife(30, 50)
+    x = game.GameOfLife()
+    x.set_map_size(30, 100)
+    x.set_density(1000)
     x.start()
